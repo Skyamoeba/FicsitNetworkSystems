7cdebc5d10a7bbc8f6600519f55912d4bb8cb292
David J <8024927+Skyamoeba@users.noreply.github.com>
2021-06-05 13:08:02 +0100

    Updated program see changes for more info v1.2
    -- updated Round() to RaidenR compact way of rounding
    -- Changed how the status data processed is done and displayed (now in a function which should make adding batteries easier)
    -- Changed how the findcomponent name is delt with by having its name at the top (will make this example easier to turn into a function to be used in a bigger program)
    --  Added batteryMaxIndicatorLevel

--- BatteryExample.lua
+++ BatteryExample.lua
@@ -1,51 +1,56 @@
 --Battery Example
+pver="1.2" -- Example Ver
+mver="0.2.1" -- Mod Ver this works with
 
+Name= "Battery" -- Name of the battery (default is "Battery")
 
---Battery Example V1.0 / ModVer 0.2.1
-function round(x) -- Used to round up for the outputs otherwise they would be 0.00415464898494948.
-local f = math.floor(x)
- if (x == f) or (x % 2.0 == 0.5) then 
-  return f
- else 
+--## Start of program ##
+function round(x) -- Used to round up for the outputs otherwise they would be 0.00415464898494948. Thanks to RaidenR for compact ver
   return math.floor(x + 0.5)
- end
 end
 
 --Battery Connection
-battery = component.proxy(component.findComponent("Battery")[1])
+battery = component.proxy(component.findComponent(Name)[1])
 -- Battery Functions
 Stored     = battery.powerStore -- Shows Stored Percentage
---StoredPer  = battery.powerStorePercent -- shows something else don't know what
+StoredPer  = battery.powerStorePercent -- shows something else don't know what
 TimeFull   = battery.timeUntilFull -- Time till full in Secs
 TimeEmpty  = battery.timeUntilEmpty -- Time till Empty In Secs
 Incoming   = battery.powerIn -- Shows the power coming in for charging
 Outgoing   = battery.powerOut -- Shows the power going out
 Capacity   = battery.powerCapacity -- Shows Battery Capacity
 Status     = battery.batteryStatus -- Shows the state the battery is in values 0 - 4 (see below for what the output number means)
+MaxIndLvl  = battery.batteryMaxIndicatorLevel -- Battery Max Indicator Level
+
+function Bat_Status(data)
+ if Status == 0 then 
+  return "Idle      "
+   elseif Status == 1 then 
+    return"Idle Empty"
+     elseif Status == 2 then 
+      return"Idle Full "
+      elseif Status == 3 then 
+     return"Power In  "
+    elseif Status == 4 then 
+   return"Power Out "
+  else 
+ return"Unknown Status" -- in case there is an update to the status this will shown that it does not know what it is.
+end
+end
+-- ## end of program ##
 
 
--- Example Print
+-- Example data outputs to Print
+print("Battery Example pv"..pver.." mv"..mver)
 print("The below displayed is in order of the Battery functions")
 print("")
-print("Stored    : "..round(Stored).."%")
+print("Stored             : "..round(Stored).."%")
 --print(round(StoredPer)) -- Dont know what this one is thought this would be % stored but it's not
-print("Time Full : "..round(TimeFull))
-print("Time Empty: "..round(TimeEmpty))
-print("Power In  : "..round(Incoming))
-print("Power Out : "..round(Outgoing))
-print("Power Capacity : "..Capacity)
-
-print(Status) -- Below is the different states this has
-if Status == 0 then 
-  print("Idle      ")
-   elseif Status == 1 then 
-    print("Idle Empty")
-     elseif Status == 2 then 
-      print("Idle Full ")
-      elseif Status == 3 then 
-     print("Power In  ")
-    elseif Status == 4 then 
-   print("Power Out ")
-  else 
- print("Unknown Status") -- in case there is an update to the status this will shown that it does not know what it is.
-end
+print("Time Full          : "..round(TimeFull))
+print("Time Empty         : "..round(TimeEmpty))
+print("Power In           : "..round(Incoming))
+print("Power Out          : "..round(Outgoing))
+print("Power Capacity     : "..Capacity)
+print("Max Indicator Lvl  : "..MaxIndLvl)
+print("Raw Status Data    : "..Status) -- Below is the different states this has
+print("Stat Data Processed: "..Bat_Status(data))
