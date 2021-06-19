0d98cc53c73b246c8cc61203416210ba1804712e
David J <8024927+Skyamoeba@users.noreply.github.com>
2021-06-19 19:43:06 +0100

    Create ItemList.lua

---
+++ ItemList.lua
@@ -0,0 +1,137 @@
+-- ITEM LIST ############################################################################################
+                  ListVer = "4.0.0"
+-- Stacks,Display Name, ConErr, LigErr, PwrErr, RadioActive 1Y 0N, System Name 
+                      VAL = {100 ,"Default                     ",0,0,0,0,"Default"}
+---- Ores ----------------------------------------------------------------------------------------------9
+                LimeStone = {100 ,"LimeStone                   ",0,0,0,0,"LimeStone"} 
+                  IronOre = {100 ,"Iron Ore                    ",0,0,0,0,"IronOre"}
+                CopperOre = {100 ,"Copper Ore                  ",0,0,0,0,"CopperOre"}
+              CateriumOre = {100 ,"Caterium Ore                ",0,0,0,0,"CateriumOre"}
+                     Coal = {100 ,"Coal                        ",0,0,0,0,"Coal"}
+                RawQuartz = {100 ,"Raw Quartz                  ",0,0,0,0,"RawQuartz"}
+                   Sulfur = {100 ,"Sulfur                      ",0,0,0,0,"Sulfur"}
+                  Bauxite = {100 ,"Bauxite                     ",0,0,0,0,"Bauxite"}
+                   SAMOre = {100 ,"SAMOre                      ",0,0,0,0,"SAMOre"}
+                  Uranium = {100 ,"Uranium                     ",0,0,0,0,"Uranium"}
+---- Ingots --------------------------------------------------------------------------------------------5
+                IronIngot = {100 ,"Iron Ingot                  ",0,0,0,0,"IronIngot"}
+              CopperIngot = {100 ,"Copper Ingot                ",0,0,0,0,"CopperIngot"}
+            CateriumIngot = {100 ,"Caterium Ingot              ",0,0,0,0,"CateriumIngot"}
+               SteelIngot = {100 ,"Steel Ingot                 ",0,0,0,0,"SteelIngot"}
+            AluminumIngot = {100 ,"Aluminum Ingot              ",0,0,0,0,"AluminumIngot"}
+---- Materials ----------------------------------------------------------------------------------------25
+                 Concrete = {500 ,"Concrete                    ",0,0,0,0,"Concrete"}
+            QuartzCrystal = {100 ,"Quartz Crystal              ",0,0,0,0,"QuartzCrystal"}
+             PolymerResin = {200 ,"PolymerResin                ",0,0,0,0,"PolymerResin"}
+            PetroleumCoke = {200 ,"PetroleumCoke               ",0,0,0,0,"PetroleumCoke"} 
+            AluminumScrap = {500 ,"Aluminum Scrap              ",0,0,0,0,"AluminumScrap"}
+                   Silica = {100 ,"Silica                      ",0,0,0,0,"Silica"}
+              BlackPowder = {100 ,"Black Powder                ",0,0,0,0,"BlackPowder"}
+                     Wire = {500 ,"Wire                        ",0,0,0,0,"Wire"}
+                    Cable = {200 ,"Cable                       ",0,0,0,0,"Cable"}
+                 IronRods = {200 ,"Iron Rods                   ",0,0,0,0,"IronRods"}
+                   Screws = {500 ,"Screws                      ",0,0,0,0,"Screws"}
+               IronPlates = {200 ,"Iron Plates                 ",0,0,0,0,"IronPlates"}
+      ReinforcedIronPlate = {100 ,"Reinforced Iron Plate       ",0,0,0,0,"ReinforcedIronPlate"}
+              CopperSheet = {200 ,"Copper Sheet                ",0,0,0,0,"CopperSheet"}
+      AlcladAluminumSheet = {100 ,"Alclad Aluminum Sheet       ",0,0,0,0,"AlcladAluminumSheet"}   
+                  Plastic = {200 ,"Plastic                     ",0,0,0,0,"Plastic"}                 
+                   Rubber = {200 ,"Rubber                      ",0,0,0,0,"Rubber"}
+            PackagedWater = {100 ,"Packaged Water              ",0,0,0,0,"PackagedWater"}
+                SteelPipe = {100 ,"Steel Pipe                  ",0,0,0,0,"SteelPipe"}
+                SteelBeam = {100 ,"Steel Beam                  ",0,0,0,0,"SteelBeam"}
+    EncasedIndustrialBeam = {100 ,"Encased Industrial Beam     ",0,0,0,0,"EncasedIndustrialBeam"}
+           GreenPowerSlug = {50  ,"Green Power Slug            ",0,0,0,0,"GreenPowerSlug"}
+          YellowPowerSlug = {50  ,"Yellow Power Slug           ",0,0,0,0,"YellowPowerSlug"}
+          PurplePowerSlug = {50  ,"Purple Power Slug           ",0,0,0,0,"PurplePowerSlug"}
+             FlowerPetals = {200 ,"FlowerPetals                ",0,0,0,0,"FlowerPetals"}
+-- Components -----------------------------------------------------------------------------------------22
+        CrystalOscillator = {100 ,"Crystal Oscillator          ",0,0,0,0,"CrystalOscillator"}
+  SuperpositionOscillator = {100 ,"Superposition Oscillator    ",0,0,0,0,"SuperpositionOscillator"}
+            EmptyCanister = {100 ,"Empty Canister              ",0,0,0,0,"EmptyCanister"}
+                   Fabric = {100 ,"Fabric                      ",0,0,0,0,"Fabric"}
+             ModularFrame = {50  ,"Modular Frame               ",0,0,0,0,"ModularFrame"}
+        HeavyModularFrame = {50  ,"Heavy Modular Frame         ",0,0,0,0,"HeavyModularFrame"}
+        FusedModularFrame = {50  ,"Fused Modular Frame         ",0,0,0,0,"FusedModularFrame"}
+                    Rotor = {100 ,"Rotor                       ",0,0,0,0,"Rotor"}
+                   Stator = {100 ,"Stator                      ",0,0,0,0,"Stator"}
+                    Motor = {50  ,"Motor                       ",0,0,0,0,"Motor"}
+                Quickwire = {500 ,"Quickwire                   ",0,0,0,0,"Quickwire"}
+             CircuitBoard = {200 ,"CircuitBoard                ",0,0,0,0,"CircuitBoard"}
+           AluminumCasing = {100 ,"Aluminum Casing             ",0,0,0,0,"AluminumCasing"}  
+                 Computer = {50  ,"Computer                    ",0,0,0,0,"Computer"}
+                AILimiter = {100 ,"A.I. Limiter                ",0,0,0,0,"AILimiter"}
+       HighSpeedConnector = {100 ,"High Speed Connector        ",0,0,0,0,"HighSpeedConnector"}
+            Supercomputer = {50  ,"Supercomputer               ",0,0,0,0,"Supercomputer"}
+                  Battery = {100 ,"Battery                     ",0,0,0,0,"Battery"}
+                 HeatSink = {100 ,"HeatSink                    ",0,0,0,0,"HeatSink"}
+         RadioControlUnit = {50  ,"Radio Control Unit          ",0,0,0,0,"RadioControlUnit"}
+               TurboMotor = {50  ,"Turbo Motor                 ",0,0,0,0,"TurboMotor"}
+ElectromagneticControlRod = {100 ,"Electromagnetic Control Rod ",0,0,0,0,"ElectromagneticControlRod"}
+            UraniumPellet = {200 ,"Uranium Pellet              ",0,0,0,0,"UraniumPellet"}
+       EncasedUraniumCell = {200 ,"Encased Uranium Cell        ",0,0,0,0,"EncasedUraniumCell"}
+                   Beacon = {100 ,"Beacon                      ",0,0,0,0,"Beacon"}
+-- Fuels ----------------------------------------------------------------------------------------------12
+            CompactedCoal = {100 ,"Compacted Coal              ",0,0,0,0,"CompactedCoal"}
+                   Leaves = {500 ,"Leaves                      ",0,0,0,0,"Leaves"}
+                  Mycelia = {200 ,"Mycelia                     ",0,0,0,0,"Mycelia"}
+                     Wood = {100 ,"Wood                        ",0,0,0,0,"Wood"}
+                  Biomass = {200 ,"Biomass                     ",0,0,0,0,"Biomass"}
+              PackagedOil = {100 ,"PackagedOil                 ",0,0,0,0,"PackagedOil"}
+  PackagedHeavyOilResidue = {100 ,"Packaged Heavy Oil Residue  ",0,0,0,0,"PackagedHeavyOilResidue"}
+             SolidBiofuel = {200 ,"SolidBiofuel                ",0,0,0,0,"SolidBiofuel"}
+             PackagedFuel = {100 ,"PackagedFuel                ",0,0,0,0,"PackagedFuel"}
+    PackagedLiquidBiofuel = {100 ,"PackagedLiquidBiofuel       ",0,0,0,0,"PackagedLiquidBiofuel"}
+        PackagedTurbofuel = {100 ,"PackagedTurbofuel           ",0,0,0,0,"PackagedTurbofuel"}
+  PackagedAluminaSolution = {50  ,"Packaged Alumina Solution   ",0,0,0,0,"PackagedAluminaSolution"}
+     PackagedSulfuricAcid = {50  ,"Packaged Sulfuric Acid      ",0,0,0,0,"PackagedSulfuricAcid"}
+       PackagedNitricAcid = {50  ,"Packaged Nitric Acid        ",0,0,0,0,"PackagedNitricAcid"}
+      PackagedNitrogenGas = {50  ,"Packaged Nitrogen Gas       ",0,0,0,0,"PackagedNitrogenGas"}
+           NuclearFuelRod = {50  ,"Nuclear Fuel Rod            ",0,0,0,1,"NuclearFuelRod"}
+-- Ammo ------------------------------------------------------------------------------------------------6
+                 Nobelisk = {50  ,"Nobelisk                    ",0,0,0,0,"Nobelisk"}
+                GasFilter = {50  ,"Gas Filter                  ",0,0,0,0,"GasFilter"}
+           ColorCartridge = {200 ,"Color Cartridge             ",0,0,0,0,"ColorCartridge"}
+           RifleCartridge = {100 ,"Rifle Cartridge             ",0,0,0,0,"RifleCartridge"}
+              SpikedRebar = {50  ,"Spiked Rebar                ",0,0,0,0,"SpikedRebar"} 
+      IodineInfusedFilter = {50  ,"Iodine Infused Filter       ",0,0,0,0,"IodineInfusedFilter"} 
+-- Special----------------------------------------------------------------------------------------------8
+               PowerShard = {100 ,"Power Shard                 ",0,0,0,0,"PowerShard"}
+             FICSITCoupon = {500 ,"FICSIT Coupon               ",0,0,0,0,"FICSITCoupon"}
+             SmartPlating = {50  ,"Smart Plating               ",0,0,0,0,"SmartPlating"}
+       VersatileFrameWork = {50  ,"Versatile FrameWork         ",0,0,0,0,"VersatileFrameWork"}
+          AutomatedWiring = {50  ,"Automated Wiring            ",0,0,0,0,"AutomatedWiring"}
+            ModularEngine = {50  ,"Modular Engine              ",0,0,0,0,"ModularEngine"}
+      AdaptiveControlUnit = {50  ,"Adaptive Control Unit       ",0,0,0,0,"AdaptiveControlUnit"}
+    MagneticFeldGenerator = {50  ,"Magnetic Feld Generator     ",0,0,0,0,"MagneticFeldGenerator"}  -- Update 4 
+   AssemblyDirectorSystem = {50  ,"Assembly Director System    ",0,0,0,0,"AssemblyDirectorSystem"} -- Update 4 
+  ThermalPropulsionRocket = {50  ,"Thermal Propulsion Rocket   ",0,0,0,0,"ThermalPropulsionRocket"} -- Update 4 
+        NonFissileUranium = {500 ,"Non-Fissile Uranium         ",0,0,0,1,"NonFissileUranium"}
+             UraniumWaste = {500 ,"Uranium Waste               ",0,0,0,1,"UraniumWaste"}
+           PlutoniumWaste = {500 ,"Plutonium Waste             ",0,0,0,1,"PlutoniumWaste"} -- Update 4
+-- Liquids ----------------------------------------------------------------------------------------------
+                     Fuel = {400 ,"Fuel                        ",0,0,0,0,"Fuel"}
+                  BioFuel = {400 ,"Bio Fuel                    ",0,0,0,0,"BioFuel"}
+                TurboFuel = {400 ,"Turbo Fuel                  ",0,0,0,0,"TurboFuel"}
+                    Water = {2400 ,"Water                       ",0,0,0,0,"Water"}
+                      Oil = {400 ,"Oil                         ",0,0,0,0,"Oil"}
+          HeavyOilResidue = {400 ,"Heavy Oil Residue           ",0,0,0,0,"HeavyOilResidue"}
+          AluminaSolution = {400 ,"Alumina Solution            ",0,0,0,0,"AluminaSolution"}
+             SulfuricAcid = {400 ,"SulfuricAcid                ",0,0,0,0,"SulfuricAcid"}
+-- Gases ------------------------------------------------------------------------------------------------
+                 Nitrogen = {2400 ,"Nitrogen                    ",0,0,0,0,"Nitrogen"} -- UPDATE 4
+-- Holiday Events ---------------------------------------------------------------------------------------
+-- FICS*MAS
+              FICSMASGift = {500 ,"FICSMAS Gift                ",0,0,0,0,"FICSMASGift"}
+        FICSMASTreeBranch = {500 ,"FICSMAS Tree Branch         ",0,0,0,0,"FICSMASTreeBranch"}
+                CandyCane = {500 ,"Candy Cane                  ",0,0,0,0,"CandyCane"}
+               FICSMASBow = {500 ,"FICSMAS Bow                 ",0,0,0,0,"FICSMASBow"}
+    CopperFICSMASOrnament = {200 ,"Copper FICSMAS Ornament     ",0,0,0,0,"CopperFICSMASOrnament"}
+       RedFICSMASOrnament = {500 ,"Red FICSMAS Ornament        ",0,0,0,0,"RedFICSMASOrnament"}
+      BlueFICSMASOrnament = {500 ,"Blue FICSMAS Ornament       ",0,0,0,0,"BlueFICSMASOrnament"}
+      IronFICSMASOrnament = {200 ,"Iron FICSMAS Ornament       ",0,0,0,0,"IronFICSMASOrnament"}
+    FICSMASOrnamentBundle = {100 ,"FICSMAS Ornament Bundle     ",0,0,0,0,"FICSMASOrnamentBundle"}
+        FICSMASDecoration = {100 ,"FICSMAS Decoration          ",0,0,0,0,"FICSMASDecoration"}
+        FICSMASWonderStar = {50  ,"FICSMAS Wonder Star         ",0,0,0,0,"FICSMASWonderStar"}
+               ActualSnow = {500 ,"Actual Snow                 ",0,0,0,0,"ActualSnow"}
+--#######################################################################################################
